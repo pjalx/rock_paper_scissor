b7e8dae4325530e31b3f18b75a3f4dca7d70923a
Paul Alexander <myotheremail@oh.rr.com>
2019-09-21 15:56:35 -0400

    Add playRound function, finished play a single round steps

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,1 +1,34 @@
-alert('script hookedup');
+function computerPlay() {
+    const compSel = [`Rock`, `Paper`, `Scissor`];
+    const ranNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    return compSel[ranNum];
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log(`The computer selects ${computerSelection}.`)
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if(playerSelection == computerSelection) {
+        return `It's a tie, ${playerSelection} = ${computerSelection}.`
+    }
+    else if(playerSelection == `Rock`) {
+        if(computerSelection == `Paper`) {
+            return `You lose, ${computerSelection} beats ${playerSelection}!`
+        }else {
+            return `You win, ${playerSelection} beats ${computerSelection}!`
+        }
+    }
+    else if(playerSelection == `Paper`) {
+        if(computerSelection == `Scissor`) {
+            return `You lose, ${computerSelection} beats ${playerSelection}!`
+        }else {
+            return `You win, ${playerSelection} beats ${computerSelection}!`
+        }
+    }
+    else if(playerSelection == `Scissor`) {
+        if(computerSelection == `Rock`) {
+            return `You lose, ${computerSelection} beats ${playerSelection}!`
+        }else {
+            return `You win, ${playerSelection} beats ${computerSelection}!`
+        }
+    }    
+}
