86f4bbd64e44e7ecb1aa4fc6c37256da17e483c4
Paul Alexander <myotheremail@oh.rr.com>
2019-09-22 16:03:01 -0400

    Add game function with round based play

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -3,8 +3,8 @@ function computerPlay() {
     const ranNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
     return compSel[ranNum];
 }
-
-function playRound(playerSelection, computerSelection) {
+/*
+function playRound(playerSelection, computerSelection = computerPlay()) {
     console.log(`The computer selects ${computerSelection}.`)
     playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
     if(playerSelection == computerSelection) {
@@ -31,4 +31,59 @@ function playRound(playerSelection, computerSelection) {
             return `You win, ${playerSelection} beats ${computerSelection}!`
         }
     }    
+}
+*/
+function game(games) {
+    if(!games) {
+        return;
+    }
+    
+    let playerWins = 0;
+    let compWins = 0;
+    let ties = 0;
+    let gamesPlayed = null;
+
+    function playRound() {
+        if(games == gamesPlayed) {
+            return console.log(`The Computer Won:${compWins} games, You Won:${playerWins} games, and Tied:${ties} times.`);
+        } 
+        gamesPlayed++;       
+        let playerSelection = prompt(`Rock Paper or Scissor, Please Choose One`, ``);
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+        const computerSelection = computerPlay();
+        console.log(`The computer selects ${computerSelection}.`)
+        if(playerSelection == computerSelection) {
+            console.log(`It's a tie, ${playerSelection} = ${computerSelection}.`);
+            ties++;
+        }
+        else if(playerSelection == `Rock`) {
+            if(computerSelection == `Paper`) {
+                console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+                compWins++;
+            }else {
+                console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+                playerWins++;
+            }
+        }
+        else if(playerSelection == `Paper`) {
+            if(computerSelection == `Scissor`) {
+                console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+                compWins++;
+            }else {
+                console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+                playerWins++;
+            }
+        }
+        else if(playerSelection == `Scissor`) {
+            if(computerSelection == `Rock`) {
+                console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+                compWins++;
+            }else {
+                console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+                playerWins++;
+            }
+        };
+        playRound();    
+    }
+    playRound();
 }
