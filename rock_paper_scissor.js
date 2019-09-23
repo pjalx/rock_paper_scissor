bb33bd1efe9e05d92e7dfd994e0790a5371f3c20
Paul Alexander <myotheremail@oh.rr.com>
2019-09-23 19:13:17 -0400

    Add more prompt check for user input

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -34,59 +34,63 @@ function playRound(playerSelection, computerSelection = computerPlay()) {
 }
 */
 function game(games) {
-    if(!games) {
+    if (!games) {
         return;
     }
-    
+
     let playerWins = 0;
     let compWins = 0;
     let ties = 0;
     let gamesPlayed = null;
 
     function playRound() {
-        if(games == gamesPlayed) {
+        if (games == gamesPlayed) {
             return console.log(`The Computer Won:${compWins} games, You Won:${playerWins} games, and Tied:${ties} times.`);
-        } 
-        gamesPlayed++;       
+        }
+        gamesPlayed++;
         let playerSelection = prompt(`Rock Paper or Scissor, Please Choose One`, ``);
         playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-        while (playerSelection != `Rock` && playerSelection != `Paper` && playerSelection != `Scissor`) {
+        while (playerSelection != `Rock` && playerSelection != `Paper` && playerSelection != `Scissor` && playerSelection != `rock` && playerSelection != `paper` && playerSelection != `scissor`) {
             playerSelection = prompt(`Please Choose Either Rock, Paper or Scissor`);
         }
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
         const computerSelection = computerPlay();
         console.log(`The computer selects ${computerSelection}.`)
-        if(playerSelection == computerSelection) {
-            console.log(`It's a tie, ${playerSelection} = ${computerSelection}.`);
+        if (playerSelection == computerSelection) {
+            console.log(`The player selects ${playerSelection}.
+            It's a tie, ${playerSelection} = ${computerSelection}.`);
             ties++;
         }
-        else if(playerSelection == `Rock`) {
-            if(computerSelection == `Paper`) {
+        else if (playerSelection == `Rock`) {
+            if (computerSelection == `Paper`) {
+                console.log(`The player selects ${playerSelection}.
+                You lose, ${computerSelection} beats ${playerSelection}!`);
+                compWins++;
+            } else {
+                console.log(`The player selects ${playerSelection}.
+                You win, ${playerSelection} beats ${computerSelection}!`);
+                playerWins++;
+            }
+        }
+        else if (playerSelection == `Paper`) {
+            if (computerSelection == `Scissor`) {
                 console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
                 compWins++;
-            }else {
+            } else {
                 console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
                 playerWins++;
             }
         }
-        else if(playerSelection == `Paper`) {
-            if(computerSelection == `Scissor`) {
+        else if (playerSelection == `Scissor`) {
+            if (computerSelection == `Rock`) {
                 console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
                 compWins++;
-            }else {
-                console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
-                playerWins++;
-            }
-        }
-        else if(playerSelection == `Scissor`) {
-            if(computerSelection == `Rock`) {
-                console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
-                compWins++;
-            }else {
+            } else {
                 console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
                 playerWins++;
             }
         };
-        playRound();    
+        playRound();
     }
     playRound();
 }
