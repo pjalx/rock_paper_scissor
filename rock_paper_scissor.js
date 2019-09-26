3cae5b7c01df81e0188ed9abb50dc8cecb61c750
Paul Alexander <myotheremail@oh.rr.com>
2019-09-26 13:12:00 -0400

    Remove commented function, no longer needed

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -3,36 +3,7 @@ function computerPlay() {
     const ranNum = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
     return compSel[ranNum];
 }
-/*
-function playRound(playerSelection, computerSelection = computerPlay()) {
-    console.log(`The computer selects ${computerSelection}.`)
-    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-    if(playerSelection == computerSelection) {
-        return `It's a tie, ${playerSelection} = ${computerSelection}.`
-    }
-    else if(playerSelection == `Rock`) {
-        if(computerSelection == `Paper`) {
-            return `You lose, ${computerSelection} beats ${playerSelection}!`
-        }else {
-            return `You win, ${playerSelection} beats ${computerSelection}!`
-        }
-    }
-    else if(playerSelection == `Paper`) {
-        if(computerSelection == `Scissor`) {
-            return `You lose, ${computerSelection} beats ${playerSelection}!`
-        }else {
-            return `You win, ${playerSelection} beats ${computerSelection}!`
-        }
-    }
-    else if(playerSelection == `Scissor`) {
-        if(computerSelection == `Rock`) {
-            return `You lose, ${computerSelection} beats ${playerSelection}!`
-        }else {
-            return `You win, ${playerSelection} beats ${computerSelection}!`
-        }
-    }    
-}
-*/
+
 function game(games) {
     if (!games) {
         return;
