35578a43f77be088e998b2a897897a186ed904df
Paul Alexander <myotheremail@oh.rr.com>
2019-10-05 12:31:48 -0400

    Add Scoreboard, CSS to style scoreboard, javascript to update scoreboard

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,10 +1,7 @@
 let playerWins = 0;
 let compWins = 0;
-let ties = 0;
+let draws = 0;
 let gamesPlayed = 0;
-const compselect = document.getElementById('compselect');
-const updateGames = document.getElementById('gamesplayed');
-
 
 function computerPlay() {
     const compSel = [`Rock`, `Paper`, `Scissor`];
@@ -14,43 +11,49 @@ function computerPlay() {
 
 function playRound(e) {
     gamesPlayed++;
-    
+    let resultText = document.getElementById('result');
     const playerSelection = e.target.value;
     const computerSelection = computerPlay();
-    console.log(`The computer selects ${computerSelection}.`)
-    compselect.innerText = `The computer selects ${computerSelection}.`;
+    const playerScoreText = document.getElementById('userscore');
+    const computerScoreText = document.getElementById('computerscore');
+    const drawsText = document.getElementById('draws');
+    const updateGames = document.getElementById('gamesplayed');
+    resultText.innerText = `The computer selects ${computerSelection}.`;
     if (playerSelection == computerSelection) {
-        console.log(`It's a tie, ${playerSelection} = ${computerSelection}.`);
-        ties++;
+        resultText.innerText += ` It's a tie, ${playerSelection} = ${computerSelection}.`;
+        draws++;
     }
     else if (playerSelection == `Rock`) {
         if (computerSelection == `Paper`) {
-            console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+            resultText.innerText += ` You lose, ${computerSelection} beats ${playerSelection}!`;
             compWins++;
         } else {
-            console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+            resultText.innerText += ` You win, ${playerSelection} beats ${computerSelection}!`;
             playerWins++;
         }
     }
     else if (playerSelection == `Paper`) {
         if (computerSelection == `Scissor`) {
-            console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+            resultText.innerText += ` You lose, ${computerSelection} beats ${playerSelection}!`;
             compWins++;
         } else {
-            console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+            resultText.innerText += ` You win, ${playerSelection} beats ${computerSelection}!`;
             playerWins++;
         }
     }
     else if (playerSelection == `Scissor`) {
         if (computerSelection == `Rock`) {
-            console.log(`You lose, ${computerSelection} beats ${playerSelection}!`);
+            resultText.innerText += ` You lose, ${computerSelection} beats ${playerSelection}!`;
             compWins++;
         } else {
-            console.log(`You win, ${playerSelection} beats ${computerSelection}!`);
+            resultText.innerText += ` You win, ${playerSelection} beats ${computerSelection}!`;
             playerWins++;
         }
     };
     updateGames.innerText = gamesPlayed;
+    drawsText.innerText = draws;
+    playerScoreText.innerText = playerWins;
+    computerScoreText.innerText = compWins;
 }
 
 document.querySelectorAll('.but').forEach(button => button.addEventListener('click', playRound));
